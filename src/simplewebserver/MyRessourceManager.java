99e326bca053a98ce329648b7b337e0aba89f46b
vengith <vengith@LJ-NB>
2014-04-29 15:17:57 +0200

    blupp

--- src/simplewebserver/MyRessourceManager.java
+++ src/simplewebserver/MyRessourceManager.java
@@ -51,8 +51,7 @@ public class MyRessourceManager implements RessourceManager {
 				copyFile(f, response);
 			}
 		} else { // Wenn NOT_FOUND
-			System.out
-					.println("Weder Datei noch Verzeichnis oder existiert nicht");
+			System.out.println("Verzeichnis und Datei nicht vorhanden");
 			response.setHttpStatus(HttpStatus.NOT_FOUND);
 			response.setContentType(ContentType.HTML);
 			response.sendHeader();
@@ -72,7 +71,7 @@ public class MyRessourceManager implements RessourceManager {
 			response.setHttpStatus(HttpStatus.OK);
 			doGet(request, response);
 		} else { // Wenn keine GET Request
-			System.out.println("Ist nicht  GET");
+			System.out.println("Kein GET-Request!");
 			response.setHttpStatus(HttpStatus.BAD_REQUEST);
 			response.sendHeader();
 			response.addToBody("<!DOCTYPE HTML> \n <html> <body> <h1>Keine gültige GET Request. Nur GET Requests werden akzeptiert! <h1> </body> </html>");
@@ -117,10 +116,10 @@ public class MyRessourceManager implements RessourceManager {
 			System.out.println("Vom Typ PNG");
 			response.setContentType(ContentType.PNG);
 			break;
-		// case "ICO":
-		// System.out.println("Vom Typ ICO");
-		// response.setContentType(ContentType.ICO);
-		// break;
+		case "ICO":
+			System.out.println("Vom Typ ICO");
+			response.setContentType(ContentType.ICO);
+			break;
 		default:
 			System.out.println("Ubekannter Typ");
 			break;
@@ -182,17 +181,17 @@ public class MyRessourceManager implements RessourceManager {
 	}
 
 	// Gibt die Dateigroesse aus
-	protected static float Dateigroesse(String Pfad) {
-		File groesse = new File(Pfad);
-		return groesse.length() / 1024;
-	}
+//	protected static float Dateigroesse(String Pfad) {
+//		File groesse = new File(Pfad);
+//		return groesse.length() / 1024;
+//	}
 
 	// Gibt zuletzt geandert aus
-	protected static String zuletztGeandert(String Pfad) {
-		File datum = new File(Pfad);
-		SimpleDateFormat dat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
-		return dat.format(datum.lastModified());
-	}
+//	protected static String zuletztGeandert(String Pfad) {
+//		File datum = new File(Pfad);
+//		SimpleDateFormat dat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+//		return dat.format(datum.lastModified());
+//	}
 
 	@Override
 	public void copyStreams(InputStream in, OutputStream out)
@@ -215,16 +214,16 @@ public class MyRessourceManager implements RessourceManager {
 		// Stringbuilder um HTML Seite zu bauen
 		StringBuilder htmlstringbuilder = new StringBuilder();
 		htmlstringbuilder
-				.append("<!DOCTYPE HTML> \n <html> \n <head>  <link rel=\"icon\"  href=\"favicon.ico\" /> \n <title>Simple Web Server</title> \n <meta name=\"author\" content=\"Alex, Fabi, Yannick\">  \n ");
+				.append("<!DOCTYPE HTML> \n <html> \n <head>  <link rel=\"icon\"  href=\"favicon.ico\" /> \n <title>Run A Server</title> \n <meta name=\"author\" content=\"Alex, Fabi, Yannick\">  \n ");
 		htmlstringbuilder
 				.append("<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"/> </head> \n");
 		// htmlstringbuilder.append("<style type=\"text/css\"> * {font-family: sans-serif;margin:0; padding:0;} h1, h2, p {padding:0.5rem;} header { display:block; background-color:#D0DAFD; color: #039;margin-top: 1em;} #wrapper {width: 800px; margin: 0px auto;} table {width: 100%; border-collapse: collapse;} td, th {padding: 0.5rem;text-align: left;} th {background-color: #D0DAFD;border-top: 4px solid #AABCFE;border-bottom: 2px solid #FFF;color: #039;} tr:hover td {background-color: #D0DAFD;color: #339;} td {background-color:#E8EDFF;border-bottom: 2px solid #FFF;color: #669;border-top: 1px solid transparent;} tr:nth-child(even) {background-color: white;} tr:nth-child(odd) {background-color: red;} a:link,a:visited,a:hover,a:active{color:#339;text-decoration: none;}</style> \n");
 		htmlstringbuilder
-				.append("<body> \n <div id=\"wrapper\"> \n <header> \n <h1>HTTP Web Server</h1> \n <h2>DHBW Mosbach, 2. Semester Wirtschaftsinformatik</h2> \n ");
+				.append("<body> \n <div id=\"wrapper\"> \n <header> \n <h1>Simple Web-Server</h1> \n");
 		htmlstringbuilder
-				.append("<p><a href=\"./\"><button type=\"button\">Gehe zum Wurzelverzeichnis</button></a> </p></header> \n");
+				.append("<p><a href=\"./\"><button type=\"button\">Root-Directory</button></a> </p></header> \n");
 		htmlstringbuilder
-				.append("<table> \n <thead> \n <tr> <th>Datei</th> <th>Groesse</th> <th>Typ</th> <th>Zuletzt Geaendert</th> </tr> \n </thead> \n <tbody> \n");
+				.append("<table> \n <thead> \n <tr> <th>Datei</th> \n </thead> \n <tbody> \n");  //<th>Groesse</th> <th>Typ</th> <th>Zuletzt Geaendert</th> </tr>
 
 		// Schreiben der Links zu den Dateien/Ordnern des Verzeichnisses in HTML
 		for (int i = 0; i < farr.length; i++) {
@@ -233,10 +232,10 @@ public class MyRessourceManager implements RessourceManager {
 			String[] path = str.split("htdocs");
 			htmlstringbuilder.append("<tr> <td> <a href='"
 					+ path[1].substring(1).replace('\\', '/')
-							.replace(" ", "%20") + "'>" + f.getName()
-					+ "</a></td><td>" + Dateigroesse(f.getAbsolutePath())
-					+ " KB</td> <td>" + Dateityp(f.getName()) + " </td> <td>"
-					+ zuletztGeandert(f.getAbsolutePath()) + " </td> </tr> \n");
+							.replace(" ", "%20") + "'>" + f.getName());
+					//+ "</a></td><td>" + Dateigroesse(f.getAbsolutePath())
+					//+ " KB</td> <td>" + Dateityp(f.getName()) + " </td> <td> \n");
+					//+ zuletztGeandert(f.getAbsolutePath()) + " </td> </tr> \n");
 		}
 
 		htmlstringbuilder.append("<tbody> </table> \n"
